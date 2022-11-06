9241496145bf69b73572199fe14e6636c94789c1
dembasow98 <dastech1998@gmail.com>
2022-11-06 19:03:00 +0300

    Done with tailwindcss setup!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      <h1>Frontend setup and folder structure done.</h1>
+      <h1 className="text-3xl font-bold underline">Frontend setup and folder structure done.</h1>
     </div>
   );
 }
