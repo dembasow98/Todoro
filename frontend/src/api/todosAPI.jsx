39a514e6c4db32935b20a2cee98abaee8c88dfcc
dembasow98 <dastech1998@gmail.com>
2022-11-06 19:51:16 +0300

    Implemented the todos Axios API instance and Methods

---
+++ frontend/src/api/todosAPI.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+
+const todosAPI = axios.create({
+    baseURL: 'http://localhost:3001/api/todos',
+});
+
+export const getTodos = async () => {
+    const response = await todosAPI.get('/todos');
+    return response.data;
+}
+
+
+export const addTodo = async (todo) => {
+    return await todosAPI.post('/todos', todo);
+}
+
+
+export const updateTodo = async (todo) => {
+    return await todosAPI.patch(`/todos/${todo.id}`, todo);
+}
+
+export const deleteTodo = async ({id}) => {
+    return await todosAPI.delete(`/todos/${id}`, id);
+}
+
+
+export default todosAPI;
